0edcd0acca841f649ac0e5a57b0d4be95884b74a
Ashutoshjat <ashutoshjat13@gmail.com>
2024-01-03 14:10:34 +0530

    second commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,117 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>HTML</li>
+        <li>Css</li>
+        <li>JavaScript</li>
+        <li>React.js</li>
+        <li>Next.js</li>
+        <li>BootStrap</li>
+        <li>Tailwind</li>
+        <li>Git</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>
+          BACHELOR OF TECHNOLOGY JECRC College 2018 – 2022 | Jaipur, India CGPA:
+          7.53
+          <li>
+            HIGHER SECONDARY SCHOOL Prince School 2017 – 2018 | Sikar, India
+            PERCENTAGE: 74.4%{" "}
+          </li>
+          <li>
+            SECONDARY SCHOOL Prince School 2014 – 2015 | Sikar, india
+            PERCENTAGE: 73.5%
+          </li>
+        </li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>
+          <b>Complete Web Design Course :</b> HTML, CSS, JAVASCRIPT and Jquery
+          and Git
+        </li>
+        <li>
+          {" "}
+          <b>REACT -</b> The Complete Guide (incl Hooks, React Router, Redux)
+        </li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/image/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a Front-End web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, React, Next.js, HTML, CSS, Bootstrap and
+            Git . I am a quick learner and I am always looking to expand my
+            knowledge and skill set. I am a team player and I am excited to work
+            with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
